9083bccdc6130b8bb681f00542e5297b407985a3
Codder41 <olcayatli@gmail.com>
2026-08-10 13:40:56 +0300

    Create report.php

---
+++ report.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once "functions.php";
+
+requireLogin();
+
+?>
+<!DOCTYPE html>
+
+<html lang="tr">
+
+<head>
+
+<meta charset="UTF-8">
+
+<link rel="stylesheet" href="assets/style.css">
+
+<title>Yeni Rapor</title>
+
+</head>
+
+<body>
+
+<div class="topbar">
+
+<div class="logo">
+
+Yeni Faaliyet Raporu
+
+</div>
+
+<div class="userinfo">
+
+<a href="dashboard.php">
+
+Ana Sayfa
+
+</a>
+
+</div>
+
+</div>
+
+<div class="container">
+
+<h1>Bu sayfa bir sonraki bölümde hazırlanacak.</h1>
+
+</div>
+
+</body>
+
+</html>
