ea62a2feba45fd27d672a12708df5020bebb74b1
Codder41 <olcayatli@gmail.com>
2026-08-10 13:41:55 +0300

    Create reports.php

---
+++ reports.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once "functions.php";
+
+requireLogin();
+
+?>
+<!DOCTYPE html>
+
+<html lang="tr">
+
+<head>
+
+<meta charset="UTF-8">
+
+<link rel="stylesheet" href="assets/style.css">
+
+<title>Raporlarım</title>
+
+</head>
+
+<body>
+
+<div class="topbar">
+
+<div class="logo">
+
+Raporlarım
+
+</div>
+
+<div class="userinfo">
+
+<a href="dashboard.php">
+
+Ana Sayfa
+
+</a>
+
+</div>
+
+</div>
+
+<div class="container">
+
+Henüz rapor bulunmuyor.
+
+</div>
+
+</body>
+
+</html>
